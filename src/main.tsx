cf74ba187462e80cf79dcaee1833725142f9adce
Darlington <6.jesus.a@gmail.com>
2025-07-01 18:11:03 -0400

    se temina el proyecto

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,8 @@ import Home from './pages/Home/Home.tsx'
 import { CartProvider } from './Context/cartProvider.tsx'
 import Checkout from './pages/Chekout/Checkout.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import Login from './pages/Login/Login.tsx'
+import Dashboard from './pages/Dashboard/Dashboard.tsx'
 
 const queryClient = new QueryClient();
 
@@ -19,8 +21,13 @@ const router = createBrowserRouter([
       { path: "/checkout", element: <Checkout /> }
 
     ]
-  }
-
+  },
+  {
+    path:"/login",element:<Login/>
+  },
+  {
+    path:"/dashboard",element:<Dashboard/>
+  },
 ])
 
 createRoot(document.getElementById('root')!).render(
--- src/service/products.service.ts
+++ src/service/products.service.ts
@@ -1,4 +1,4 @@
-import type { Products } from "../interface";
+import type { Product, Products } from "../interface";
 
 export const getProducts = async (page = 1):Promise<Products[]> => {
     try {
@@ -14,3 +14,24 @@ export const getProducts = async (page = 1):Promise<Products[]> => {
         throw new Error('Network error');
     }
 }
+
+export const createProduct = async(product:Product):Promise<Product> => {
+  try {
+    const response = await fetch ('http://localhost:3000/products',{
+        method:'POST',
+        headers:{
+            'Content-Type': 'application/json' 
+        },
+        body:JSON.stringify(product)
+    }) 
+
+    if(response.ok){
+        const data = await response.json()
+        return data
+    }else{
+        throw new Error('Fail to create product')
+    }
+  } catch (error) {
+    throw new Error('Network error')
+  }
+} 
